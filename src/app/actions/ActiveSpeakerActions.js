e074e822bb4c30b62ce904faeb767c4a350741c4
Dejan Petkovic <dwecci@gmail.com>
2020-07-17 13:51:39 +0200

    Let active speaker talk at least 3 seconds

--- src/app/actions/ActiveSpeakerActions.js
+++ src/app/actions/ActiveSpeakerActions.js
@@ -4,11 +4,10 @@ export const Types = {
   START_ACTIVE_SPEAKER: "START_ACTIVE_SPEAKER",
   STOP_ACTIVE_SPEAKER: "STOP_ACTIVE_SPEAKER",
   PARTICIPANT_SPEAKING: "PARTICIPANT_SPEAKING",
-  PENDING_PARTICIPANT_SPEAKING: "PENDING_PARTICIPANT_SPEAKING",
   DISABLE_FORCE_ACTIVE_SPEAKER: "DISABLE_FORCE_ACTIVE_SPEAKER",
   FORCE_ACTIVE_SPEAKER: "FORCE_ACTIVE_SPEAKER",
+  SILENCE: "SILENCE",
 };
-const NEXT_ACTIVE_SPEAKER_DEFAULT_CNT = 3;
 
 export class Actions {
   static startActiveSpeaker() {
@@ -43,49 +42,32 @@ export class Actions {
                 ? participantsConnected[0]
                 : participants.participants.find((p) => p.isSpeaking) ||
                   null;
-            let nextActiveSpeaker = activeSpeaker.nextActiveSpeaker,
-                nextActiveSpeakerCnt = activeSpeaker.nextActiveSpeakerCnt;
             if(participant) {
-              // Set new AS if there is none
-              if(!activeSpeaker.activeSpeaker ) {
-                // console.log('Setting the first active speaker');
+              // Set new active speaker if there is none
+              if(!activeSpeaker.activeSpeaker || !activeSpeaker.activeSpeakerSince
+                  || activeSpeaker.activeSpeaker.participant_id == participant.participant_id) {
+                if(!activeSpeaker.activeSpeaker || !activeSpeaker.activeSpeakerSince)
+                  // console.log('Setting the first active speaker', participant);
                 dispatch({
                   type: Types.PARTICIPANT_SPEAKING,
                   payload: { participant },
                 });
-                dispatch(this.pendingActiveSpeaker(null, NEXT_ACTIVE_SPEAKER_DEFAULT_CNT))
               }
-              // Clear next AS if participant == active speaker
-              else if(activeSpeaker.activeSpeaker != participant && nextActiveSpeaker != participant) {
-                // console.log('Pending new active speaker', NEXT_ACTIVE_SPEAKER_DEFAULT_CNT);
-                dispatch(this.pendingActiveSpeaker(participant, NEXT_ACTIVE_SPEAKER_DEFAULT_CNT))
-              }
-              // Clear next AS if participant == active speaker
-              else if(activeSpeaker.activeSpeaker == participant) {
-                if(nextActiveSpeaker) {
-                  // console.log('Removing next active speaker');
-                  dispatch(this.pendingActiveSpeaker(null, NEXT_ACTIVE_SPEAKER_DEFAULT_CNT))
-                }
-              }
-              // Decrease next AS cnt if next AS didnt change
-              else if(nextActiveSpeaker == participant) {
-                nextActiveSpeakerCnt--;
-                if(nextActiveSpeakerCnt>0) {
-                  // console.log('Upgrading next active speaker', nextActiveSpeakerCnt);
-                  dispatch(this.pendingActiveSpeaker(participant, nextActiveSpeakerCnt))
-                } else {
-                  // Set new active speaker if  next AS cnt drops to 0
-                  // console.log('Setting new active speaker', participant);
-                  dispatch({
-                    type: Types.PARTICIPANT_SPEAKING,
-                    payload: { participant },
-                  });
-                  dispatch(this.pendingActiveSpeaker(null, NEXT_ACTIVE_SPEAKER_DEFAULT_CNT))
-                }
-              }
-            } // else {
-              // console.log('No speakers');
-            // }
+              // Set new active speaker
+              else if(activeSpeaker.activeSpeaker.participant_id != participant.participant_id &&
+                  ( !activeSpeaker.activeSpeakerSince || Date.now()-activeSpeaker.activeSpeakerSince>3000) ) {
+                // console.log('Switch to next active speaker', participant);
+                dispatch({
+                  type: Types.PARTICIPANT_SPEAKING,
+                  payload: { participant, activeSpeakerSince: Date.now() },
+                });
+              } //else {
+                // console.log('Delay next active speaker', participant);
+              //}
+            } else {
+              //console.log('No speakers');
+              dispatch(this.startSilence())
+            }
           }
         }, 500);
       }
@@ -105,10 +87,9 @@ export class Actions {
     };
   }
 
-  static pendingActiveSpeaker(participant, weight) {
+  static startSilence() {
     return {
-      type: Types.PENDING_PARTICIPANT_SPEAKING,
-      payload: { participant, weight },
+      type: Types.SILENCE,
     };
   }
 
