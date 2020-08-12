e657a8c0471ccdaedfb70f18c55ecf149ca7f435
Thomas Gilbert <thomas.gilbert@dolby.com>
2020-08-12 10:08:54 +0200

    - extract main component, - fix fex linter issue.

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import thunkMidleware from "redux-thunk";
-import promiseMiddleware from "redux-promise";
 import { combineReducers, createStore, applyMiddleware } from "redux";
 import {
-  ConferenceRoom,
   VoxeetProvider,
   reducer as voxeetReducer
 } from "./VoxeetReactComponents";
+import Main from "./components/main/Main";
 
 const reducers = combineReducers({
   voxeet: voxeetReducer
@@ -20,48 +19,10 @@ window.addEventListener("storage", function(e) {
   console.log(sessionStorage.getItem("conferenceId"));
 });
 
-const conferenceId = window.conferenceId;
-
-const settings = {
-  consumerKey: "CONSUMER_KEY",
-  consumerSecret: "CONSUMER_SECRET",
-  conferenceAlias: "conference_name"
-};
-
-const handleOnConnect = () => {
-  console.log("Participant connecting");
-};
-
-const handleOnLeave = () => {
-  console.log("Participant disconnected");
-};
-
-var constraints = {
-  audio: true,
-  video: true
-};
-
-var videoRatio = {
-  width: 1280,
-  height: 720
-};
-
 ReactDOM.render(
   <VoxeetProvider store={configureStore()}>
     <div>
-      <ConferenceRoom
-        isWidget={false}
-        autoJoin
-        videoRatio={videoRatio}
-        kickOnHangUp
-        handleOnLeave={handleOnLeave}
-        handleOnConnect={handleOnConnect}
-        constraints={constraints}
-        consumerKey={settings.consumerKey}
-        consumerSecret={settings.consumerSecret}
-        conferenceAlias={settings.conferenceAlias}
-        videoCodec={"H264"}
-      />
+    <Main/>
     </div>
   </VoxeetProvider>,
   document.getElementById("app")
