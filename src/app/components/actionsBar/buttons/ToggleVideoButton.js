b74084a3c18b378a0a51b1324f6f65f310af0a69
Dejan Petkovic <dwecci@gmail.com>
2020-10-19 15:45:29 +0200

    CCU-79 - Prevent sending toggle until response on previous call is returned

--- src/app/components/actionsBar/buttons/ToggleVideoButton.js
+++ src/app/components/actionsBar/buttons/ToggleVideoButton.js
@@ -5,18 +5,53 @@ import { strings } from "../../../languages/localizedStrings";
 import CameraOn from "../../../../static/images/icons/btn-camera-on.svg";
 import CameraOff from "../../../../static/images/icons/btn-camera-off.svg";
 import { isMobile } from "../../../libs/browserDetection";
+const TIMEOUT = 1500; // Should be a bit longer than API response
 
 class ToggleVideoButton extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isMobile: isMobile(),
-      hover: false
+      hover: false,
+      local:null  // local video state, if set contains video state on last click
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.clickTimer = null; // Allow some time for api call to return next state
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if(this.props.videoEnabled !== prevProps.videoEnabled) {
+      // Video state changed => reset local state
+      console.log('video state changed to', this.props.videoEnabled);
+      if(this.clickTimer) {
+        clearTimeout(this.clickTimer);
+        this.clickTimer = null;
+      }
+      this.setState({local:null});
+    }
+  }
+
+  handleClick(e) {
+    const { toggle, videoEnabled } = this.props;
+    const { local } = this.state;
+    console.log('video clicked', local, videoEnabled)
+    e.preventDefault();
+    // Prevent sending toggle if no response on previous click received ot timeout expires
+    if(local!==videoEnabled) {
+      // About to perform toggle
+      if(this.clickTimer)
+        clearTimeout(this.clickTimer);
+      this.clickTimer=setTimeout(() => {
+        this.setState({local:null}, () => {
+          this.clickTimer = null;
+        });
+      }, 1000)
+      this.setState({local:videoEnabled}, toggle); // Set current video state
+    }
   }
 
   render() {
-    const { videoEnabled, toggle, tooltipPlace, isBottomBar } = this.props;
+    const { videoEnabled, tooltipPlace, isBottomBar } = this.props;
     const { hover, isMobile } = this.state;
 
     return (
@@ -33,7 +68,7 @@ class ToggleVideoButton extends Component {
           data-tip
           data-for="toggle-video"
           title={strings.video}
-          onClick={() => toggle()}
+          onClick={this.handleClick}
         >
           <img src={videoEnabled || hover ? CameraOn : CameraOff} />
           {isBottomBar && (
