93a014d5d79e447e43870c0e1f3c93d1e18ab0ed
Valentin <valentin.martin33@gmail.com>
2020-12-16 14:54:29 +0100

    Add DVWC StateDump handling

--- src/app/actions/ConferenceActions.js
+++ src/app/actions/ConferenceActions.js
@@ -1705,6 +1705,7 @@ export class Actions {
   }
 
   static _conferenceJoined(conferenceId, conferencePincode, dolbyVoiceEnabled) {
+    VoxeetSDK.configureStateDump(60);
     return {
       type: Types.CONFERENCE_JOINED,
       payload: {
