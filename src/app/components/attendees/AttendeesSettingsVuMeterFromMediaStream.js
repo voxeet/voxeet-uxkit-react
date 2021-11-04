474a0746a01362f18d8e6db11b4de696325c7553
Artur Kolakowski <artur.kolakowski@dolby.com>
2021-11-04 13:38:35 +0100

    CCU-235 Safari 15 - Audio device selection is not working

---
+++ src/app/components/attendees/AttendeesSettingsVuMeterFromMediaStream.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import PreConfigVuMeter from "../preConfig/PreConfigVuMeter.js";
+
+import { connect } from "@voxeet/react-redux-5.1.1";
+
+@connect((store) => {
+  return {
+    inputManager: store.voxeet.inputManager,
+  };
+})
+class AttendeesSettingsVuMeterFromMediaStream extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userStream: null,
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.inputManager.currentAudioDevice != null)
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: { deviceId: this.props.inputManager.currentAudioDevice },
+          video: false,
+        })
+        .then((stream) => this.setState({ userStream: stream }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.inputManager.currentAudioDevice !==
+        this.props.inputManager.currentAudioDevice &&
+      this.props.inputManager.currentAudioDevice !== null
+    )
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: { deviceId: this.props.inputManager.currentAudioDevice },
+          video: false,
+        })
+        .then((stream) => this.setState({ userStream: stream }));
+  }
+
+  render() {
+    return <PreConfigVuMeter stream={this.state.userStream} maxLevel={this.props.maxLevel} />;
+  }
+}
+
+AttendeesSettingsVuMeterFromMediaStream.propTypes = {
+  maxLevel: PropTypes.number,
+};
+
+export default AttendeesSettingsVuMeterFromMediaStream;
--- src/app/components/preConfig/PreConfigVuMeter.js
+++ src/app/components/preConfig/PreConfigVuMeter.js
@@ -1,12 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { connect } from "@voxeet/react-redux-5.1.1";
+import VuMeter from "./VuMeter";
 
-@connect(store => {
-  return {
-    inputManager: store.voxeet.inputManager
-  };
-})
 class PreConfigVuMeter extends Component {
   constructor(props) {
     super(props);
@@ -15,8 +10,7 @@ class PreConfigVuMeter extends Component {
       javascriptNode: null,
       microphone: null,
       analyser: null,
-      userStream: null,
-      audioContext: null
+      audioContext: null,
     };
     this.createAnalyser = this.createAnalyser.bind(this);
   }
@@ -27,87 +21,68 @@ class PreConfigVuMeter extends Component {
       this.state.javascriptNode.onaudioprocess = null;
       this.state.javascriptNode = null;
     }
-    if (this.state.userStream)
-      this.state.userStream.getTracks().forEach(t => t.stop());
     if (this.state.microphone) this.state.microphone.disconnect();
     if (this.state.analyser) this.state.analyser.disconnect();
   }
 
-  createAnalyser() {
-    if (this.props.inputManager.currentAudioDevice != null) {
-      if (this.state.javascriptNode != null && this.state.userStream != null) {
-        this.state.javascriptNode.disconnect(
-          this.state.audioContext.destination
-        );
-        this.state.javascriptNode.onaudioprocess = null;
-        this.state.microphone.disconnect();
-        this.state.analyser.disconnect();
-        this.state.userStream.getTracks().forEach(t => t.stop());
-        this.setState({
-          level: 0,
-          javascriptNode: null,
-          microphone: null,
-          analyser: null,
-          audioContext: null,
-          userStream: null
-        });
+  async createAnalyser() {
+    if (this.props.stream == null) return;
+
+    if (this.state.javascriptNode != null) {
+      this.state.javascriptNode.disconnect(this.state.audioContext.destination);
+      this.state.javascriptNode.onaudioprocess = null;
+      this.state.microphone.disconnect();
+      this.state.analyser.disconnect();
+      this.setState({
+        level: 0,
+        javascriptNode: null,
+        microphone: null,
+        analyser: null,
+        audioContext: null,
+      });
+    }
+    try {
+      const audioCtx =
+        window.AudioContext ||
+        window.webkitAudioContext ||
+        window.mozAudioContext;
+      const audioContext = new audioCtx();
+      let analyser = audioContext ? audioContext.createAnalyser() : null;
+      let microphone = audioContext
+        ? audioContext.createMediaStreamSource(this.props.stream)
+        : null;
+      let javascriptNode = audioContext
+        ? audioContext.createScriptProcessor(2048, 1, 1)
+        : null;
+      if (analyser) {
+        javascriptNode.connect(audioContext.destination);
+        analyser.smoothingTimeConstant = 0.8;
+        analyser.fftSize = 1024;
+        microphone.connect(analyser);
+        analyser.connect(javascriptNode);
+        javascriptNode.connect(audioContext.destination);
+        javascriptNode.onaudioprocess = () => {
+          const array = new Uint8Array(analyser.frequencyBinCount);
+          analyser.getByteFrequencyData(array);
+          const sum = array.reduce((total, next) => total + next);
+          const average = sum / array.length;
+          this.setState({ level: average });
+        };
       }
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: { deviceId: this.props.inputManager.currentAudioDevice },
-          video: false
-        })
-        .then(stream => {
-          const audioCtx =
-            window.AudioContext ||
-            window.webkitAudioContext ||
-            window.mozAudioContext;
-          const audioContext = new audioCtx();
-          let analyser = audioContext? audioContext.createAnalyser():null;
-          let microphone = audioContext? audioContext.createMediaStreamSource(stream):null;
-          let javascriptNode = audioContext? audioContext.createScriptProcessor(2048, 1, 1):null;
-          if(analyser) {
-            javascriptNode.connect(audioContext.destination);
-            analyser.smoothingTimeConstant = 0.8;
-            analyser.fftSize = 1024;
-            microphone.connect(analyser);
-            analyser.connect(javascriptNode);
-            javascriptNode.connect(audioContext.destination);
-            javascriptNode.onaudioprocess = () => {
-              var array = new Uint8Array(analyser.frequencyBinCount);
-              analyser.getByteFrequencyData(array);
-              var values = 0;
-
-              var length = array.length;
-              for (var i = 0; i < length; i++) {
-                values += array[i];
-              }
-
-              var average = values / length;
-              this.setState({ level: average });
-            };
-          }
-          this.setState({
-            audioContext: audioContext,
-            microphone: microphone,
-            javascriptNode: javascriptNode,
-            userStream: stream,
-            analyser: analyser
-          });
-        }).catch((e) => {
-          console.error('Could not create analyzer', e.message);
-        });
+      this.setState({
+        audioContext: audioContext,
+        microphone: microphone,
+        javascriptNode: javascriptNode,
+        analyser: analyser,
+      });
+    } catch (e) {
+      console.error("Could not create analyzer", e.message);
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (
-      prevProps.inputManager.currentAudioDevice !=
-        this.props.inputManager.currentAudioDevice &&
-      this.props.inputManager.currentAudioDevice != null
-    ) {
+    if (this.props.stream !== null && prevProps.stream !== this.props.stream)
       this.createAnalyser();
-    }
   }
 
   componentDidMount() {
@@ -115,22 +90,13 @@ class PreConfigVuMeter extends Component {
   }
 
   render() {
-    const { level } = this.state;
-    const maxLevel = this.props.maxLevel || 22;
-    return (
-      <ul className="loadbar">
-        {[...Array(maxLevel)].map((el, i) => (
-          <li key={`loadbar_${i}`}>
-            <div className={`bar ${level >= i ? "ins" : ""}`}></div>
-          </li>
-        ))}
-      </ul>
-    );
+    return <VuMeter level={this.state.level} maxLevel={this.props.maxLevel} />;
   }
 }
 
 PreConfigVuMeter.propTypes = {
-  userStream: PropTypes.object
+  stream: PropTypes.object,
+  maxLevel: PropTypes.number,
 };
 
 export default PreConfigVuMeter;
